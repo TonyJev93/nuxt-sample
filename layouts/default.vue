b9c7ca57b9b682be183abda1fb2ba5de05b8dbff
JUN GYEWON <tonyjev93@gmail.com>
2022-04-05 09:43:44 +0900

    feat: edit layouts & add Header

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <AppHeader />
+      <Nuxt/>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+
+export default {
+  name: "defaultPage",
+  components: { AppHeader },
+}
+</script>
+
+<style>
+
+</style>
