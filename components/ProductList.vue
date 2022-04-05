1f6dd4cf17f00c06ec932ba8c6a8e0b596132b25
JUN GYEWON <tonyjev93@gmail.com>
2022-04-06 02:23:38 +0900

    fix: 오타

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-import { fetchProducts } from '~/api';
+import { fetchProduct } from '@/api';
 
 export default {
     // asyncData 는 page component 이기 때문에 pages 하위에서만 동작. 해당 함수는 동작 하지 않음.
     async asyncData() {
-        const response = await fetchProducts()
+        const response = await fetchProduct()
         console.log(response)
         const products = response.data
         return { products }
