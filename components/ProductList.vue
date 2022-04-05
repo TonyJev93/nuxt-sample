cb54498b49fafc9123baffe62b62c217390d4e05
JUN GYEWON <tonyjev93@gmail.com>
2022-04-05 17:40:49 +0900

    feat: 상품 상세 & 검색 binding

---
+++ components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{ products }}
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    // asyncData 는 page component 이기 때문에 pages 하위에서만 동작. 해당 함수는 동작 하지 않음.
+    async asyncData() {
+        const response = await axios.get('http://localhost:3000/products')
+        console.log(response)
+        const products = response.data
+        return { products }
+    },
+    name: 'ProductList',
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/SearchInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <!-- <input type="text" :value="searchKeyword" @input="$emit('input', $event.target.value)"/> -->
+      <input type="text" :value="value" @input="$emit('input', $event.target.value)"/>
+      <button>search</button>
+  </div>
+</template>
+
+<script>
+export default {
+    // model: {
+        // props 변수명 다르게 하고싶을 때 사용. 
+        // (기본적으로 v-model 사용 시 props 는 value 를 써야함.)
+    // },
+    props: {
+        value: {
+            type: String,
+            default: () => '',
+        },
+        searchKeyword: {
+            type: String,
+            default: () => '',
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
